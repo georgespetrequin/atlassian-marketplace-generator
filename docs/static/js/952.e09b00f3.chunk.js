ebd98d5af4d6077b8f12e6febd947b72670a92ca
Georges <georgespetrequin@gmail.com>
2025-02-26 22:22:48 +0000

    Update docs folder for GitHub Pages

---
+++ docs/static/js/952.e09b00f3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkatlassian_marketplace_preview_builder=self.webpackChunkatlassian_marketplace_preview_builder||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.e09b00f3.chunk.js.map
